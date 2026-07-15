96660488debd9e928aa89179168d60736cefd178
TPLSAHIL98 <sahilofficial824@gmail.com>
2026-07-15 16:47:00 +0530

    Implement NovaAI mod initializer with logging

--- src/main/java/com/sahil/novaai/NovaAI.java
+++ src/main/java/com/sahil/novaai/NovaAI.java
@@ -1,1 +1,16 @@
+package com.sahil.novaai;
 
+import net.fabricmc.api.ModInitializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class NovaAI implements ModInitializer {
+
+    public static final String MOD_ID = "novaai";
+    public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+    @Override
+    public void onInitialize() {
+        LOGGER.info("NovaAI loaded successfully!");
+    }
+}
